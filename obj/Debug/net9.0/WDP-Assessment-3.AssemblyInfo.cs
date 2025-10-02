b5a6c728b38f32eeabf067b1b8f34cf971452294
Boryslavir <u3295557@uni.canberra.edu.au>
2025-10-02 13:57:41 +1000

    add contact page

--- obj/Debug/net9.0/WDP-Assessment-3.AssemblyInfo.cs
+++ obj/Debug/net9.0/WDP-Assessment-3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WDP-Assessment-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1b721aeed339916e384f5bdff965fc9904c5fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WDP-Assessment-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WDP-Assessment-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
