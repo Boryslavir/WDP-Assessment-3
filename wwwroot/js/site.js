3bde6a4e6ac32e0f1c3c2761209b2d2d35bbc7c2
Boryslavir <u3295557@uni.canberra.edu.au>
2025-10-06 22:57:22 +1100

    update layout for <Home> page (part 1)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById('body-dalle-card').addEventListener('mouseover', function(tag) {
+    if (tag.target.tagName === 'IMG') {
+        this.innerHTML = `<p>DALL-E 3</p>`;
+    }
+});
+
+document.getElementById('body-dalle-card').addEventListener('mouseout', function(tag) {
+    if (!this.contains(tag.relatedTarget)) {
+        this.innerHTML = '<img src="/img/dalle.webp" alt="DALL-E Logo" />';
+    }
+});
+
+document.getElementById('body-midjourney-card').addEventListener('mouseover', function(tag) {
+    if (tag.target.tagName === 'IMG') {
+        this.innerHTML = `<p>Mid Journey</p>`;
+    }
+});
+
+document.getElementById('body-midjourney-card').addEventListener('mouseout', function(tag) {
+    if (!this.contains(tag.relatedTarget)) {
+        this.innerHTML = '<img src="/img/midjourney.webp" alt="MidJourney Logo" />';
+    }
+});
+
